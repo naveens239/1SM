7dc99cb7a0155662eadc5671003e07639abc4ec2
Naveena <ndavids239@gmail.com>
2014-12-03 22:27:26 +0530

    Initial commit

---
+++ start.bat
@@ -0,0 +1,7 @@
+cls
+echo "                         *  *  *  *  *  *  *  *  *  *"
+echo "                       *                              *"
+echo " * * * * * * * * * * *     1 S t o p M a k e . c o m      * * * * * * * * * * *"
+echo "                       *                              *"
+echo "                         *  *  *  *  *  *  *  *  *  *"
+nodemon --watch client\partials -e html
---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo "                         *  *  *  *  *  *  *  *  *  *"
+echo "                       *                              *"
+echo " * * * * * * * * * * *     1 S t o p M a k e . c o m      * * * * * * * * * * *"
+echo "                       *                              *"
+echo "                         *  *  *  *  *  *  *  *  *  *"
+nodemon --watch client/partials -e html
