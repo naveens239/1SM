3e10a96eaa162b5d1ef6ceea834c6163fd4def68
Naveena <ndavids239@gmail.com>
2014-12-05 13:15:46 +0530

    changed URL and IP to 1stopmake.com

--- server/modules/config/config.base.js
+++ server/modules/config/config.base.js
@@ -13,17 +13,17 @@ module.exports = {
         "facebook": {
             "clientID"    : "813703648669257",
             "clientSecret": "39427539054aa777d7dfb76fe743e3b3",
-            "callbackURL" : "http://1stopwed.com/auth/facebook/callback"
+            "callbackURL" : "http://1stopmake.com/auth/facebook/callback"
         },
         "twitter" : {
             "consumerKey"   : "39427539054aa777d7dfb76fe743e3b3",
             "consumerSecret": "j9hp6iYQZWBMFus5lYCZ7db0KmhMylNpOWUYA7ar8isoWk0tdb",
-            "callbackURL"   : "http://1stopwed.com/auth/twitter/callback"
+            "callbackURL"   : "http://1stopmake.com/auth/twitter/callback"
         },
         "google"  : {
             "clientID": "44376929588-eucb7ibmgft6m098t8f5afiajok36edb.apps.googleusercontent.com",
             "clientSecret"    : "AOrF78uP90X5qDPHvPqBRHfq",
-            "callbackURL"   : "http://1stopwed.com/auth/google/callback"
+            "callbackURL"   : "http://1stopmake.com/auth/google/callback"
         }
     }
 }
--- server/modules/config/config.production.js
+++ server/modules/config/config.production.js
@@ -2,6 +2,6 @@ module.exports = {
     port    : 80,
     //NOTE: can only override the entire top level object
     dbconfig: {
-        dburl             : "167.88.41.30:27017/1SM"
+        dburl             : "128.199.130.78:27017/1SM"
     }
 };
